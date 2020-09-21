fb50e18aace659a187f3c10c65e029e2bcced8cf
Reuben Miller <reuben.d.miller@gmail.com>
2020-09-21 21:02:09 +0200

    Fixing panic when parsing invalid relative date (#11)
    * updating tparse dependency to improve handling of panics for invalid intervals
    * adding unittest for date parsing

--- pkg/cmd/timestampHelper.go
+++ pkg/cmd/timestampHelper.go
@@ -16,7 +16,6 @@ func parseDurationRelativeToNow(offsetDuration string) (*time.Time, error) {
 
 // getTimestampUsingOffset returns a timestamp relative to a base timestamp
 // example: +1d3w4mo-7y6h4m
-// TODO: an offsetDuration of "30" throws a panic!
 func getTimestampUsingOffset(now time.Time, offsetDuration string) (*time.Time, error) {
 	another, err := tparse.AddDuration(now, offsetDuration)
 	if err != nil {
