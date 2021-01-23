bab68df612e7ec12395d8828c8b4d80a5f189ba4
reubenmiller <reuben.d.miller@gmail.com>
2021-01-23 21:06:00 +0000

    adding delay before checking events

--- tools/PSc8y/Tests/Update-EventBinary.manual.Tests.ps1
+++ tools/PSc8y/Tests/Update-EventBinary.manual.Tests.ps1
@@ -11,12 +11,14 @@ Describe -Name "Update-EventBinary" {
         $ExpectedText1 = "my test file`nsecond line"
         $ExpectedText1 | Out-File $TestFile
         PSc8y\New-EventBinary -Id $Event.id -File $TestFile
+        Start-Sleep -Seconds 1
         $BinaryContents = (PSc8y\Get-EventBinary -Id $Event.id) -join "`n"
         $BinaryContents | Should -BeExactly $ExpectedText1
 
         $ExpectedText2 = "adjusted text äöüß"
         $ExpectedText2 | Out-File $TestFile
         $BinaryContents = PSc8y\Update-EventBinary -Id $Event.id -File $TestFile
+        Start-Sleep -Seconds 1
         $BinaryContents = (PSc8y\Get-EventBinary -Id $Event.id) -join "`n"
         $BinaryContents | Should -BeExactly $ExpectedText2
     }
