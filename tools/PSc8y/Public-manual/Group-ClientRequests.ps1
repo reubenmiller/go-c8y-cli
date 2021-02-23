a8102d874d2fa88ecc960d988bf3276d751bcf02
reubenmiller <reuben.d.miller@gmail.com>
2021-02-23 07:32:06 +0000

    adding support for grouping items into given sizes rather than always waiting for the entire input

--- tools/PSc8y/Public-manual/Group-ClientRequests.ps1
+++ tools/PSc8y/Public-manual/Group-ClientRequests.ps1
@@ -1,8 +1,13 @@
 function Group-ClientRequests {
-    [CmdletBinding(
-    )]
+<# 
+.SYNOPSIS
+Groups the input into array of a given maximum size. It will pass the piped input as array rather than individual items
+#>
+    [CmdletBinding()]
     param (
         # Input objects to be piped to native c8y binary
+        [AllowNull()]
+        [AllowEmptyCollection()]
         [Parameter(
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true,
@@ -13,26 +18,37 @@ function Group-ClientRequests {
         [object[]]
         $InputObject,
 
+        [int]
+        $Size = 2000,
+
         [switch]
         $AsPSObject
     )
 
     begin {
-        $items = New-Object System.Collections.ArrayList
+        $Buffer = New-Object System.Collections.ArrayList
     }
 
     process {
         foreach ($item in $InputObject) {
-            if ($AsPSObject -or $item -is [string]) {
-                $pipeitem = $item 
+            if ($AsPSObject -or $item -is [string] -or $item -is [int] -or $item -is [long]) {
+                $pipeitem = $item
             } else {
                 $pipeitem = ConvertTo-Json -InputObject $item -Depth 100 -Compress
             }
-            $null = $items.Add($pipeitem)
+            [void]$Buffer.Add($pipeitem)
+
+            if ($Buffer.Count -eq $Size) {
+                $b = $Buffer;
+                $Buffer = New-Object System.Collections.ArrayList($Size)
+                ,$b
+            }
         }
     }
 
     end {
-        Write-Output $items -NoEnumerate
+        if ($Buffer.Count -ne 0) {  
+            ,$Buffer
+        }
     }
 }
