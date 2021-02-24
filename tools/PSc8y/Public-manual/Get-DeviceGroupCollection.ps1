cbd44c8dcc983f96c9bd1e94026fb1fa3dd702b2
reubenmiller <reuben.d.miller@gmail.com>
2021-02-24 19:40:07 +0000

    refactoring to use new cmdlet structure

--- tools/PSc8y/Public-manual/Get-DeviceGroupCollection.ps1
+++ tools/PSc8y/Public-manual/Get-DeviceGroupCollection.ps1
@@ -57,127 +57,37 @@ Get a list of devices groups which have been created more recently than 2020-01-
         # Include a flat list of all parents and grandparents of the given object
         [Parameter()]
         [switch]
-        $WithParents,
-
-        # Maximum number of results
-        [Parameter()]
-        [AllowNull()]
-        [AllowEmptyString()]
-        [ValidateRange(1,2000)]
-        [int]
-        $PageSize,
-
-        # Include total pages statistic
-        [Parameter()]
-        [switch]
-        $WithTotalPages,
-
-        # Get a specific page result
-        [Parameter()]
-        [int]
-        $CurrentPage,
-
-        # Maximum number of pages to retrieve when using -IncludeAll
-        [Parameter()]
-        [int]
-        $TotalPages,
-
-        # Include all results
-        [Parameter()]
-        [switch]
-        $IncludeAll,
-
-        # Include raw response including pagination information
-        [Parameter()]
-        [switch]
-        $Raw,
-
-        # Write the response to file
-        [Parameter()]
-        [string]
-        $OutputFile,
-
-        # Ignore any proxy settings when running the cmdlet
-        [Parameter()]
-        [switch]
-        $NoProxy,
-
-        # Specifiy alternative Cumulocity session to use when running the cmdlet
-        [Parameter()]
-        [string]
-        $Session,
-
-        # TimeoutSec timeout in seconds before a request will be aborted
-        [Parameter()]
-        [double]
-        $TimeoutSec
+        $WithParents
     )
+    DynamicParam {
+        Get-ClientCommonParameters -Type "Get", "Collection" -BoundParameters $PSBoundParameters
+    }
 
     Begin {
-        $Parameters = @{}
-        if ($PSBoundParameters.ContainsKey("Name")) {
-            $Parameters["name"] = $Name
-        }
-        if ($PSBoundParameters.ContainsKey("Type")) {
-            $Parameters["type"] = $Type
-        }
-        if ($PSBoundParameters.ContainsKey("FragmentType")) {
-            $Parameters["fragmentType"] = $FragmentType
-        }
-        if ($PSBoundParameters.ContainsKey("owner")) {
-            $Parameters["owner"] = $Owner
-        }
-        if ($PSBoundParameters.ContainsKey("Query")) {
-            $Parameters["query"] = $Query
-        }
-        if ($PSBoundParameters.ContainsKey("ExcludeRootGroup")) {
-            $Parameters["excludeRootGroup"] = $ExcludeRootGroup
-        }
-        if ($PSBoundParameters.ContainsKey("WithParents")) {
-            $Parameters["withParents"] = $WithParents
-        }
-        if ($PSBoundParameters.ContainsKey("PageSize")) {
-            $Parameters["pageSize"] = $PageSize
-        }
-        if ($PSBoundParameters.ContainsKey("WithTotalPages") -and $WithTotalPages) {
-            $Parameters["withTotalPages"] = $WithTotalPages
-        }
-        if ($PSBoundParameters.ContainsKey("OutputFile")) {
-            $Parameters["outputFile"] = $OutputFile
-        }
-        if ($PSBoundParameters.ContainsKey("NoProxy")) {
-            $Parameters["noProxy"] = $NoProxy
-        }
-        if ($PSBoundParameters.ContainsKey("Session")) {
-            $Parameters["session"] = $Session
-        }
-        if ($PSBoundParameters.ContainsKey("TimeoutSec")) {
-            $Parameters["timeout"] = $TimeoutSec * 1000
+        if ($env:C8Y_DISABLE_INHERITANCE -ne $true) {
+            # Inherit preference variables
+            Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
         }
 
+        $c8yargs = New-ClientArgument -Parameters $PSBoundParameters -Command "devices listDeviceGroups"
+        $ClientOptions = Get-ClientOutputOption $PSBoundParameters
+        $TypeOptions = @{
+            Type = "application/vnd.com.nsn.cumulocity.customDeviceGroupCollection+json"
+            ItemType = "application/vnd.com.nsn.cumulocity.customDeviceGroup+json"
+            BoundParameters = $PSBoundParameters
+        }
     }
 
     Process {
-        if (!$Force -and
-            !$WhatIfPreference -and
-            !$PSCmdlet.ShouldProcess(
-                (PSc8y\Get-C8ySessionProperty -Name "tenant"),
-                (Format-ConfirmationMessage -Name $PSCmdlet.MyInvocation.InvocationName -InputObject "")
-            )) {
-            continue
-        }
 
-        Invoke-ClientCommand `
-            -Noun "devices" `
-            -Verb "listDeviceGroups" `
-            -Parameters $Parameters `
-            -Type "application/vnd.com.nsn.cumulocity.customDeviceGroupCollection+json" `
-            -ItemType "application/vnd.com.nsn.cumulocity.customDeviceGroup+json" `
-            -ResultProperty "managedObjects" `
-            -Raw:$Raw `
-            -CurrentPage:$CurrentPage `
-            -TotalPages:$TotalPages `
-            -IncludeAll:$IncludeAll
+        if ($ClientOptions.ConvertToPS) {
+            c8y devices listDeviceGroups $c8yargs `
+            | ConvertFrom-ClientOutput @TypeOptions
+        }
+        else {
+            c8y devices listDeviceGroups $c8yargs
+        }
     }
 
+    End {}
 }
