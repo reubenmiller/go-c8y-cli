7219884d521f21b0bce80b4a253c997fe6abcef1
reubenmiller <reuben.d.miller@gmail.com>
2021-02-26 21:33:57 +0000

    refactoring to use new common options

--- tools/PSc8y/Public-manual/Invoke-ClientRequest.ps1
+++ tools/PSc8y/Public-manual/Invoke-ClientRequest.ps1
@@ -1,5 +1,5 @@
 ﻿Function Invoke-ClientRequest {
-<#
+    <#
 .SYNOPSIS
 Send a rest request using the c8y
 
@@ -77,19 +77,6 @@ Create a new managed object but add a custom accept header value
         [ValidateSet("GET", "POST", "DELETE", "PUT", "HEAD")]
         [Microsoft.PowerShell.Commands.WebRequestMethod] $Method = 'GET',
 
-        # Request body
-        [object] $Data,
-
-        # Template (jsonnet) file to use to create the request body.
-        [Parameter()]
-        [string]
-        $Template,
-
-        # Variables to be used when evaluating the Template. Accepts a file path, json or json shorthand, i.e. "name=peter"
-        [Parameter()]
-        [string]
-        $TemplateVars,
-
         # Add custom headers to the rest request
         [hashtable] $Headers,
 
@@ -105,180 +92,84 @@ Create a new managed object but add a custom accept header value
         # Accept header
         [string] $Accept,
 
-        # Ignore the accept header
-        [switch]
-        $IgnoreAcceptHeader,
-
-        # Timeout in seconds
-        [Parameter()]
-        [double]
-        $Timeout,
-
-        # Compress the json response (i.e. opposite of pretty print)
-        [Parameter()]
-        [switch]
-        $Compress,
-
-        # Pretty print the json response (i.e. opposite of compress print). Takes precedence over Compress
-        [Parameter()]
-        [switch]
-        $Pretty,
-
-        # Include raw response including pagination information
-        [Parameter()]
-        [switch]
-        $Raw,
-
-        # Outputfile
-        [Parameter()]
-        [string]
-        $OutputFile,
-
-        # NoProxy
-        [Parameter()]
-        [switch]
-        $NoProxy,
-
-        # Session path
-        [Parameter()]
-        [string]
-        $Session,
-
         # HostName to use which overrides the given host
-        [string] $HostName,
-
-        # Allow loading Cumulocity session setting from environment variables
-        [Parameter()]
-        [switch]
-        $UseEnvironment
+        [string] $HostName
     )
-
-    $c8y = Get-ClientBinary
-
-    $c8yargs = New-Object System.Collections.ArrayList
-
-    $null = $c8yargs.Add("rest")
-
-    if ($Method) {
-        $null = $c8yargs.Add($Method)
+    DynamicParam {
+        Get-ClientCommonParameters -Type "Create", "Template" -BoundParameters $PSBoundParameters
     }
 
-    if ($null -ne $QueryParameters) {
-        $queryparams = New-Object System.Collections.ArrayList
-        foreach ($key in $QueryParameters.Keys) {
-            $value = $QueryParameters[$key]
-            if ($value) {
-                $null = $queryparams.Add("${key}=${value}")
+    Begin {
+
+        if ($env:C8Y_DISABLE_INHERITANCE -ne $true) {
+            # Inherit preference variables
+            Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        }
+
+        $c8yargs = New-ClientArgument -Parameters $PSBoundParameters -Command "inventory create" -Exclude @(
+            "Uri", "Method", "Headers", "QueryParameters", "InFile", "HostName"
+        )
+        $ClientOptions = Get-ClientOutputOption $PSBoundParameters
+        $TypeOptions = @{
+            Type = if ($Accept) { $Accept } else { "application/json" }
+            ItemType = ""
+            BoundParameters = $PSBoundParameters
+        }
+    }
+
+    Process {
+
+        if ($Method) {
+            $null = $c8yargs.Add($Method)
+        }
+
+        if ($null -ne $QueryParameters) {
+            $queryparams = New-Object System.Collections.ArrayList
+            foreach ($key in $QueryParameters.Keys) {
+                $value = $QueryParameters[$key]
+                if ($value) {
+                    $null = $queryparams.Add("${key}=${value}")
+                }
+            }
+
+            if ($queryparams.Count -gt 0) {
+                $str = $queryparams -join "&"
+                if ($Uri.Contains("?")) {
+                    # uri already has some query parameters, so just append the new one to it
+                    $Uri = $Uri + "&" + $str
+                }
+                else {
+                    $Uri = $Uri + "?" + $str
+                }
             }
         }
 
-        if ($queryparams.Count -gt 0) {
-            $str = $queryparams -join "&"
-            if ($Uri.Contains("?")) {
-                # uri already has some query parameters, so just append the new one to it
-                $Uri = $Uri + "&" + $str
-            } else {
-                $Uri = $Uri + "?" + $str
+        $null = $c8yargs.Add($Uri)
+
+        if ($null -ne $Headers) {
+            foreach ($key in $Headers.Keys) {
+                $null = $c8yargs.AddRange(@("-H=`"{0}: {1}`"" -f $key, $Headers[$key]))
             }
         }
-    }
 
-    $null = $c8yargs.Add($Uri)
-
-    if ($null -ne $Data -and ![string]::IsNullOrEmpty($Data)) {
-        if ($Data -is [string]) {
-            if (Test-Json -InputObject $Data -WarningAction SilentlyContinue) {
-                $null = $c8yargs.AddRange(@("--data", (ConvertTo-JsonArgument $Data)))
-            } else {
-                # allow shortform strings (intepreted by c8y cli tool)
-                $null = $c8yargs.AddRange(@("--data", $Data))
-            }
-        } else {
-            # Convert hashtables, psobject etc.
-            $null = $c8yargs.AddRange(@("--data", (ConvertTo-JsonArgument $Data)))
+        if ($HostName) {
+            $null = $c8yargs.AddRange(@("--host", $HostName))
         }
 
-    }
+        if ($InFile) {
+            $null = $c8yargs.AddRange(@("--file", $InFile))
+        }
 
-    if (-not [string]::IsNullOrEmpty($Template)) {
-        $null = $c8yargs.AddRange(@("--template", $Template))
-    }
-
-    if (-not [string]::IsNullOrEmpty($TemplateVars)) {
-        $null = $c8yargs.AddRange(@("--templateVars", $TemplateVars))
-    }
-
-    if ($null -ne $Headers) {
-        foreach ($key in $Headers.Keys) {
-            $null = $c8yargs.AddRange(@("-H=`"{0}: {1}`"" -f $key, $Headers[$key]))
+        if ($ClientOptions.ConvertToPS) {
+            $Name `
+            | Group-ClientRequests `
+            | c8y rest $c8yargs `
+            | ConvertFrom-ClientOutput @TypeOptions
+        }
+        else {
+            $Name `
+            | Group-ClientRequests `
+            | c8y rest $c8yargs
         }
     }
-
-    if (-not [string]::IsNullOrEmpty($ContentType)) {
-        $null = $c8yargs.AddRange(@("--contentType", $ContentType))
-    }
-
-    if (-not [string]::IsNullOrEmpty($Accept)) {
-        $null = $c8yargs.AddRange(@("--accept", $Accept))
-    }
-
-    if ($IgnoreAcceptHeader) {
-        $null = $c8yargs.Add("--ignoreAcceptHeader")
-    }
-
-    if ($HostName) {
-        $null = $c8yargs.AddRange(@("--host", $HostName))
-    }
-
-    if ($Timeout) {
-        $null = $c8yargs.AddRange(@("--timeout", $Timeout))
-    }
-
-    if ($InFile) {
-        $null = $c8yargs.AddRange(@("--file", $InFile))
-    }
-
-    if ($OutputFile) {
-        $null = $c8yargs.AddRange(@("--outputFile", $OutputFile))
-    }
-
-    if ($Raw) {
-        $null = $c8yargs.Add("--raw")
-    }
-
-    if ($Session) {
-        $null = $c8yargs.AddRange(@("--session", $Session))
-    }
-
-    if ($UseEnvironment) {
-        $null = $c8yargs.Add("--useEnv")
-    }
-
-    if ($NoProxy) {
-        $null = $c8yargs.Add("--noProxy")
-    }
-
-
-    if ($PSBoundParameters.ContainsKey("Pretty")) {
-        $null = $c8yargs.Add("--compact={0}" -f (!$Pretty).ToString().ToLower())
-    } elseif ($PSBoundParameters.ContainsKey("Compress")) {
-        if ($Compress) {
-            $null = $c8yargs.Add("--compact={0}" -f $Compress.ToString().ToLower())
-        }
-    }
-
-    # Always use verbose messages so the output can be parsed by PowerShell
-    $null = $c8yargs.Add("--verbose")
-
-    # Don't use colours as it can interfere with log parsing
-    $null = $c8yargs.Add("--noColor")
-
-    if ($WhatIfPreference) {
-        $null = $c8yargs.Add("--dry")
-    }
-
-    Write-Verbose ("{0} {1}" -f $c8y, ($c8yargs -join " "))
-
-    $output = & $c8y $c8yargs 2>&1
-    Write-ClientMessage $output -PassThru
 }
