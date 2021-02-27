e5178b154dbc20a6c65c882a8d1df737332747da
reubenmiller <reuben.d.miller@gmail.com>
2021-02-27 11:05:41 +0000

    refactoring cmdlet

--- tools/PSc8y/Tests/New-ServiceUser.manual.Tests.ps1
+++ tools/PSc8y/Tests/New-ServiceUser.manual.Tests.ps1
@@ -15,6 +15,20 @@ Describe -Name "New-ServiceUser" -Tag "microservice" {
         $Response.requiredRoles | Should -BeExactly $Roles
     }
 
+    It "Creates a server user and subscribes to multiple tenants" {
+        $Roles = @("ROLE_INVENTORY_READ", "ROLE_INVENTORY_ADMIN")
+        $Response = PSc8y\New-ServiceUser -Name $AppName -Roles $Roles -Tenants t1111,t2222 -WhatIf 2>&1
+        $LASTEXITCODE | Should -Be 0
+        $Response | Should -Not -BeNullOrEmpty
+
+        $Response | Should -ContainRequest "POST /application/applications" -Total 1
+        $Response | Should -ContainRequest "POST /tenant/tenants/t1111/applications" -Total 1
+        $Response | Should -ContainRequest "POST /tenant/tenants/t2222/applications" -Total 1
+        $Bodies = Get-RequestBodyCollection $Response
+        $Bodies[0].name | Should -BeExactly $AppName
+        $Bodies[0].requiredRoles | Should -BeExactly $Roles
+    }
+
     It "Creates a new microservice service user application with more than 1 role" {
         $Roles = @("ROLE_INVENTORY_READ", "ROLE_INVENTORY_ADMIN")
         $Response = PSc8y\New-ServiceUser -Name $AppName -Roles $Roles
