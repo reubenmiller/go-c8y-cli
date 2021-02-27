4006040ad49988c3ab3393cf69112f307eaa778c
reubenmiller <reuben.d.miller@gmail.com>
2021-02-27 08:28:17 +0000

    adding common parameter SilentStatusCodes

--- tools/PSc8y/Public-manual/Get-ClientCommonParameters.ps1
+++ tools/PSc8y/Public-manual/Get-ClientCommonParameters.ps1
@@ -91,6 +91,7 @@ Inherit common parameters to a custom function. This will add parameters such as
 
         # Error options
         New-DynamicParam -Name WithError -Type "switch" -DPDictionary $Dictionary
+        New-DynamicParam -Name SilentStatusCodes -Type "string" -DPDictionary $Dictionary
 
         # Workers
         New-DynamicParam -Name Workers -Type "int" -DPDictionary $Dictionary
