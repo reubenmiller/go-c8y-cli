351a038f0081c60472386ee681832e0c6591c3fd
reubenmiller <reuben.d.miller@gmail.com>
2021-03-07 21:58:36 +0000

    refactoring test

--- tools/PSc8y/Tests/New-ServiceUser.manual.Tests.ps1
+++ tools/PSc8y/Tests/New-ServiceUser.manual.Tests.ps1
@@ -17,16 +17,18 @@ Describe -Name "New-ServiceUser" -Tag "microservice" {
 
     It "Creates a server user and subscribes to multiple tenants" {
         $Roles = @("ROLE_INVENTORY_READ", "ROLE_INVENTORY_ADMIN")
-        $Response = PSc8y\New-ServiceUser -Name $AppName -Roles $Roles -Tenants t1111,t2222 -WhatIf 2>&1
+        $output = PSc8y\New-ServiceUser -Name $AppName -Roles $Roles -Tenants t1111,t2222 -WhatIf -WhatIfFormat json 2>&1
         $LASTEXITCODE | Should -Be 0
-        $Response | Should -Not -BeNullOrEmpty
+        $output | Should -Not -BeNullOrEmpty
+        $Requests = $output | ConvertFrom-Json
 
-        $Response | Should -ContainRequest "POST /application/applications" -Total 1
-        $Response | Should -ContainRequest "POST /tenant/tenants/t1111/applications" -Total 1
-        $Response | Should -ContainRequest "POST /tenant/tenants/t2222/applications" -Total 1
-        $Bodies = Get-RequestBodyCollection $Response
-        $Bodies[0].name | Should -BeExactly $AppName
-        $Bodies[0].requiredRoles | Should -BeExactly $Roles
+        $Requests | Should -HaveCount 3
+        $Requests[0] | Should -MatchObject @{method="POST"; path="/application/applications"} -Property method, path
+        $Requests[1] | Should -MatchObject @{method="POST"; path="/tenant/tenants/t1111/applications"} -Property method, path
+        $Requests[2] | Should -MatchObject @{method="POST"; path="/tenant/tenants/t2222/applications"} -Property method, path
+
+        $Requests[0].body.name | Should -BeExactly $AppName
+        $Requests[0].body.requiredRoles | Should -BeExactly $Roles
     }
 
     It "Creates a new microservice service user application with more than 1 role" {
