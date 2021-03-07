47835b58b4b83a37816013f3b2693709a4f8ce30
reubenmiller <reuben.d.miller@gmail.com>
2021-03-07 22:08:31 +0000

    updating manual dry tests

--- tools/PSc8y/Tests/c8y/dry.manual.Tests.ps1
+++ tools/PSc8y/Tests/c8y/dry.manual.Tests.ps1
@@ -4,8 +4,9 @@ Describe -Name "c8y dry" {
 
     Context "multi-form data" {
         It "Displays multi form data" {
-
-            $output = c8y binaries create --file=./Makefile --dry
+            $tempFile = New-TemporaryFile
+            "äüText" | Out-File $tempFile
+            $output = c8y binaries create --file=$tempFile --dry 2>&1
             $LASTEXITCODE | Should -Be 0
             $output | Should -Not -BeNullOrEmpty
         }
@@ -14,24 +15,24 @@ Describe -Name "c8y dry" {
     Context "POST" {
         It "Shows the body contents" {
 
-            $output = c8y devices create --name test01 --dry
+            $output = c8y devices create --name test01 --dry --withError
             $LASTEXITCODE | Should -Be 0
             $output | Should -Not -BeNullOrEmpty
         }
 
         It "Shows the body contents with a custom body" {
 
-            $output = c8y devices create --name test01 --data "test=1" --dry
+            $output = c8y devices create --name test01 --data "test=1" --dry 2>&1
             $LASTEXITCODE | Should -Be 0
             $output | Should -Not -BeNullOrEmpty
         }
 
         It "Hides sensitive information" {
             $env:C8Y_LOGGER_HIDE_SENSITIVE = "true"
-            $output = c8y devices create --name test01 --data "test=1" --dry
+            $output = c8y devices create --name test01 --data "test=1" --dry 2>&1
             $LASTEXITCODE | Should -Be 0
             $output | Should -Not -BeNullOrEmpty
-            $output | Should -Not -Match 
+            $output | Should -Not -Match $env:C8Y_USERNAME
 
             $env:C8Y_LOGGER_HIDE_SENSITIVE = $null
         }
