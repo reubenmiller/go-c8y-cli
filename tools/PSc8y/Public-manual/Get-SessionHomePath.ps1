5259347c430ae83cf776e967a5570eaeb3a334fe
reubenmiller <reuben.d.miller@gmail.com>
2021-03-11 20:02:02 +0000

    using native command to get session home path

--- tools/PSc8y/Public-manual/Get-SessionHomePath.ps1
+++ tools/PSc8y/Public-manual/Get-SessionHomePath.ps1
@@ -1,26 +1,18 @@
 Function Get-SessionHomePath {
+<# 
+.SYNOPSIS
+Get session home path
+
+.DESCRIPTION
+Get the path where all sessions are stored
+
+.Link
+c8y settings list
+
+.EXAMPLE
+Get-SessionHomePath 
+#>
     [cmdletbinding()]
     Param()
-
-    $c8yFolderName = ".cumulocity"
-
-    if ($env:C8Y_SESSION_HOME) {
-        $HomePath = $env:C8Y_SESSION_HOME
-    }
-    elseif ($HOME) {
-        # Use PS Automatic Variable
-        $HomePath = Join-Path $HOME -ChildPath $c8yFolderName
-    }
-    else {
-        # Check if on windows (PS 5.1)
-        $IsWindowsOS = !($IsMacOS -or $IsLinux)
-        if ($IsWindowsOS -and $env:HOMEDRIVE -and $env:HOMEPATH) {
-            $HomePath = Join-Path -Path "$Env:HOMEDRIVE\$Env:HOMEPATH" -ChildPath $c8yFolderName
-        } else {
-            # default to current directory
-            $HomePath = Join-Path "." -ChildPath $c8yFolderName
-        }
-    }
-
-    $HomePath
+    c8y settings list --select "session.home" --csv
 }
