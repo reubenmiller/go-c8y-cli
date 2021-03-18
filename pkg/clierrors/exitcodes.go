d4ef1b00e4bf4ac6c40f5a0993fbaf363f9af7fe
reubenmiller <reuben.d.miller@gmail.com>
2021-03-18 20:54:45 +0000

    adding more exit codes

--- pkg/clierrors/exitcodes.go
+++ pkg/clierrors/exitcodes.go
@@ -3,8 +3,29 @@ package clierrors
 type ExitCode int
 
 const (
-	ExitOK     ExitCode = 0
-	ExitError  ExitCode = 1
-	ExitCancel ExitCode = 2
-	ExitAuth   ExitCode = 4
+	ExitOK ExitCode = 0
+
+	// Map HTTP status codes to exit codes
+	ExitBadRequest400          ExitCode = 40
+	ExitUnauthorized401        ExitCode = 1
+	ExitForbidden403           ExitCode = 3
+	ExitNotFound404            ExitCode = 4
+	ExitMethodNotAllowed405    ExitCode = 5
+	ExitConflict409            ExitCode = 9
+	ExitExecutionTimeout413    ExitCode = 13
+	ExitInvalidData422         ExitCode = 22
+	ExitTooManyRequests429     ExitCode = 29
+	ExitInternalServerError500 ExitCode = 50
+	ExitNotImplemented501      ExitCode = 51
+	ExitBadGateway502          ExitCode = 52
+	ExitServiceUnavailable503  ExitCode = 53
+
+	ExitCancel              ExitCode = 2
+	ExitError               ExitCode = 100
+	ExitUserError           ExitCode = 101
+	ExitNoSession           ExitCode = 102
+	ExitAbortedWithErrors   ExitCode = 103
+	ExitCompletedWithErrors ExitCode = 104
+	ExitJobLimitExceeded    ExitCode = 105
+	ExitTimeout             ExitCode = 106
 )
