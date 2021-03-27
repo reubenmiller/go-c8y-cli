9aaf5e35ca6d941d1ad537cce689484ed00a4f91
reubenmiller <reuben.d.miller@gmail.com>
2021-03-27 18:47:28 +0000

    adding pipeline injection support for templates

--- pkg/cmd/template/execute/execute.manual.go
+++ pkg/cmd/template/execute/execute.manual.go
@@ -12,7 +12,6 @@ import (
 	"github.com/reubenmiller/go-c8y-cli/pkg/jsonUtilities"
 	"github.com/reubenmiller/go-c8y-cli/pkg/mapbuilder"
 	"github.com/spf13/cobra"
-	"github.com/tidwall/pretty"
 )
 
 type CmdExecute struct {
@@ -40,6 +39,12 @@ Example 2:
 $ c8y template execute --template ./mytemplate.jsonnet --templateVars "name=testme" --data "name=myDeviceName"
 
 Verify a jsonnet template and specify input data to be used as the input when evaluating the template
+
+Example 3:
+$ echo '{"name": "external_source"}' | c8y template execute --template "{name: input.value.name, nestedValues: input.value}"
+$ => {"name":"external_source","nestedValues":{"name":"external_source"}}
+
+Pass external json data into the template, and reference it via the "input.value" variable
 		`),
 		RunE: ccmd.newTemplate,
 	}
@@ -50,6 +55,7 @@ Verify a jsonnet template and specify input data to be used as the input when ev
 		cmd,
 		flags.WithData(),
 		f.WithTemplateFlag(cmd),
+		flags.WithExtendedPipelineSupport("input", "input", false, "id"),
 	)
 
 	// Required flags
@@ -80,6 +86,7 @@ func (n *CmdExecute) newTemplate(cmd *cobra.Command, args []string) error {
 		flags.WithDataFlagValue(),
 		cmdutil.WithTemplateValue(cfg),
 		flags.WithTemplateVariablesValue(),
+		flags.WithStringValue("input", "input", ""),
 	)
 
 	if err != nil {
@@ -99,13 +106,8 @@ func (n *CmdExecute) newTemplate(cmd *cobra.Command, args []string) error {
 	}
 
 	if isJSONResponse {
-		formatter := pretty.Pretty
-		if cfg.CompactJSON() {
-			formatter = pretty.UglyInPlace
-		}
-		fmt.Printf("%s%s", formatter(bytes.TrimSpace(responseText)), outputEnding)
-	} else {
-		fmt.Printf("%s%s", bytes.TrimSpace(responseText), outputEnding)
+		return n.factory.WriteJSONToConsole(cfg, cmd, "", responseText)
 	}
+	fmt.Printf("%s%s", bytes.TrimSpace(responseText), outputEnding)
 	return nil
 }
