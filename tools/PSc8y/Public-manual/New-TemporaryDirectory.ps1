62baf7bb92f4dfc3728514a49ae0fa8e26f0fdd0
reubenmiller <reuben.d.miller@gmail.com>
2021-04-07 18:11:54 +0000

    making new temp directory folder public

---
+++ tools/PSc8y/Public-manual/New-TemporaryDirectory.ps1
@@ -0,0 +1,17 @@
+Function New-TemporaryDirectory {
+<# 
+.SYNOPSIS
+Create a new temporary directory
+
+.DESCRIPTION
+Create a temporary directory in the systems temp directory folder.
+
+.EXAMPLE
+New-TemporaryDirectory
+
+Create a new temp directory
+#>
+    $parent = [System.IO.Path]::GetTempPath()
+    [string] $name = [System.Guid]::NewGuid()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
