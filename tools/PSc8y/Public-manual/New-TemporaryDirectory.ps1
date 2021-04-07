c4435da7ff511b5cb379da798fe6a9a9de0ba244
reubenmiller <reuben.d.miller@gmail.com>
2021-04-07 20:38:42 +0000

    adding cmdlet binding

--- tools/PSc8y/Public-manual/New-TemporaryDirectory.ps1
+++ tools/PSc8y/Public-manual/New-TemporaryDirectory.ps1
@@ -11,6 +11,8 @@ New-TemporaryDirectory
 
 Create a new temp directory
 #>
+    [cmdletbinding()]
+    Param()
     $parent = [System.IO.Path]::GetTempPath()
     [string] $name = [System.Guid]::NewGuid()
     New-Item -ItemType Directory -Path (Join-Path $parent $name)
