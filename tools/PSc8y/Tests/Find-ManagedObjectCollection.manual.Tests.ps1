0e4c744c435439c7b0d2afeffe63f8fc564b0180
reubenmiller <reuben.d.miller@gmail.com>
2021-04-14 21:11:49 +0000

    adding tests for piping managed object query

--- tools/PSc8y/Tests/Find-ManagedObjectCollection.manual.Tests.ps1
+++ tools/PSc8y/Tests/Find-ManagedObjectCollection.manual.Tests.ps1
@@ -12,6 +12,41 @@ Describe -Name "Find-ManagedObjectCollection" {
         $Response | Should -Not -BeNullOrEmpty
     }
 
+    It "Find all devices by piping query text" {
+        $output = "name eq '*a*'" | PSc8y\Find-ManagedObjectCollection -OrderBy "_id desc" -OnlyDevices -Dry 2>&1
+        $LASTEXITCODE | Should -Be 0
+        $requests = $output | ConvertFrom-Json
+        $requests | Should -HaveCount 1
+
+        $requests[0] | Should -MatchObject @{
+            path = "/inventory/managedObjects";
+            query = "query=`$filter=has(c8y_IsDevice) and name eq '*a*' `$orderby=_id desc"
+        } -Property path, query
+    }
+
+    It "Find matching devices by piping an object" {
+        $output = @{ "c8y_DeviceQueryString" = "name eq '*a*'"} | PSc8y\Find-ManagedObjectCollection -OrderBy "_id desc" -OnlyDevices -Dry 2>&1
+        $LASTEXITCODE | Should -Be 0
+        $requests = $output | ConvertFrom-Json
+        $requests | Should -HaveCount 1
+
+        $requests[0] | Should -MatchObject @{
+            path = "/inventory/managedObjects";
+            query = "query=`$filter=has(c8y_IsDevice) and name eq '*a*' `$orderby=_id desc"
+        } -Property path, query
+    }
+
+    It "Finds inventory managed objects using a template query" {
+        $output = @{ "c8y_DeviceQueryString" = "name eq '*a*'"} | PSc8y\Find-ManagedObjectCollection -OrderBy "_id desc" -QueryTemplate "not(%s)" -Dry 2>&1
+        $LASTEXITCODE | Should -Be 0
+        $requests = $output | ConvertFrom-Json
+        $requests | Should -HaveCount 1
+
+        $requests[0] | Should -MatchObject @{
+            path = "/inventory/managedObjects";
+            query = "query=`$filter=not(name eq '*a*') `$orderby=_id desc"
+        } -Property path, query
+    }
 
     AfterEach {
         Remove-ManagedObject -Id $Device.id
