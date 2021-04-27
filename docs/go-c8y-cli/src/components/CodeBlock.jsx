7eed0914893fef94f88d7e2f1d04c577ab0552bc
reubenmiller <reuben.d.miller@gmail.com>
2021-04-27 18:53:24 +0000

    docs: refactoring CodeBlock to support copy/paste

--- docs/go-c8y-cli/src/components/CodeBlock.jsx
+++ docs/go-c8y-cli/src/components/CodeBlock.jsx
@@ -1,11 +1,11 @@
 /* eslint react/jsx-key: 0 */
 import React from 'react';
-import Highlight, { defaultProps } from 'prism-react-renderer'
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import { mdx } from '@mdx-js/react';
 import useThemeContext from '@theme/hooks/useThemeContext';
 import lightTheme from 'prism-react-renderer/themes/github';
 import darkTheme from 'prism-react-renderer/themes/dracula';
+import Code from '@docusaurus/theme-classic/lib-next/theme/CodeBlock';
 
 
 const c8yCommands = {
@@ -130,9 +130,6 @@ function transformToPowerShell(code = '') {
 
 export default ({ children, className = 'bash', live = false, render = false, transform = false }) => {
     const { isDarkTheme } = useThemeContext();
-
-    const language = className && className.replace ? className.replace(/language-/, '') : 'bash';
-
     if (live) {
         return (
             <div style={{ marginTop: '40px' }}>
@@ -164,24 +161,16 @@ export default ({ children, className = 'bash', live = false, render = false, tr
             childrenCode = transformToPowerShell(children.trim());
         } else {
             childrenCode = children.trim();
+            if (className === 'powershell') {
+                childrenCode = childrenCode.replace(/\\/g, '`')
+            }
         }
     }
 
     const theme = isDarkTheme ? darkTheme : lightTheme;
-
     return (
-        <Highlight {...defaultProps} theme={theme} code={childrenCode} language={language}>
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={className} style={{ ...style, padding: '20px' }}>
-                    {tokens.map((line, i) => (
-                        <div key={i} {...getLineProps({ line, key: i })}>
-                            {line.map((token, key) => (
-                                <span key={key} {...getTokenProps({ token, key })} />
-                            ))}
-                        </div>
-                    ))}
-                </pre>
-            )}
-        </Highlight>
+        <Code className={className} theme={theme}>
+            {childrenCode}
+        </Code>
     )
 }
