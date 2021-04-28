169e8e7f039dc13a88cbd14aab418f9512c712c4
reubenmiller <reuben.d.miller@gmail.com>
2021-04-28 19:40:57 +0000

    adding test to cover powershell template quoting

---
+++ tools/PSc8y/Tests/c8y/c8y.template.manual.tests.ps1
@@ -0,0 +1,10 @@
+. $PSScriptRoot/../imports.ps1
+
+Describe -Name "c8y template" {
+    It "template should preservce double quotes" {
+        $output = c8y template execute --template '{\"email\": \"he ll@ex ample.com\"}'
+        $LASTEXITCODE | Should -Be 0
+        $body = $output | ConvertFrom-Json
+        $body.email | Should -MatchExactly "he ll@ex ample.com"
+    }
+}
