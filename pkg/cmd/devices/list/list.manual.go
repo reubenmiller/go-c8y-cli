357e4b24fb0894df43c3cb74e645c3a8edf7c9ce
reubenmiller <reuben.d.miller@gmail.com>
2021-04-29 21:03:49 +0000

    docs: adding more c8y devices list examples

--- pkg/cmd/devices/list/list.manual.go
+++ pkg/cmd/devices/list/list.manual.go
@@ -33,8 +33,13 @@ func NewCmdDevicesList(f *cmdutil.Factory) *CmdDevicesList {
 		Long:  `Get a collection of devices based on filter parameters`,
 		Example: heredoc.Doc(`
 		$ c8y devices list --name "sensor*" --type myType
-
 		Get a collection of devices of type "myType", and their names start with "sensor"
+
+		$ c8y devices list --query "name eq '*sensor*' and creationTime.date gt '2021-04-02T00:00:00'"
+		Get devices which names containing 'sensor' and were created after 2021-04-02
+
+		$ echo -e "c8y_MacOS\nc8y_Linux" | c8y devices list --queryTemplate "type eq '%s'"
+		Get devices with type 'c8y_MacOS' then devices with type 'c8y_Linux' (using pipeline)
 		`),
 		RunE: ccmd.RunE,
 	}
