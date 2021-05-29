b534db95b1dbff2d6560ce9824191dfa70e698da
reubenmiller <reuben.d.miller@gmail.com>
2021-05-29 17:15:26 +0000

    fix: Encrypted session check no is triggered properly when setting a session.

--- pkg/cmd/sessions/set/set.manual.go
+++ pkg/cmd/sessions/set/set.manual.go
@@ -130,7 +130,7 @@ func (n *CmdSet) RunE(cmd *cobra.Command, args []string) error {
 	n.factory.Config = func() (*config.Config, error) {
 		return cfg, nil
 	}
-	client, err := factory.CreateCumulocityClient(n.factory, "", "", "", false)()
+	client, err := factory.CreateCumulocityClient(n.factory, "", "", "", true)()
 
 	if err != nil {
 		return err
