8c171cba5d4bfd88ac18f254bd27db08b9759e18
reubenmiller <reuben.d.miller@gmail.com>
2021-05-31 12:59:26 +0000

    fix: generating c8y completions from go command instead of compiled binary

--- tools/shell/completion/build_completions.sh
+++ tools/shell/completion/build_completions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+ROOT_DIR="$( pwd )"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 OUTPUT_DIR=${OUTPUT_DIR:-"$SCRIPT_DIR"}
@@ -8,18 +9,20 @@ mkdir -p "$OUTPUT_DIR"
 pushd "$OUTPUT_DIR"
 
 echo "Cloning addons repo"
-if [[ -d .go-c8y-cli ]]; then
-    rm -Rf .go-c8y-cli
+if [[ ! -d .go-c8y-cli ]]; then
+    git clone --depth 1 https://github.com/reubenmiller/go-c8y-cli-addons.git .go-c8y-cli 2>/dev/null
+else
+    git -C .go-c8y-cli pull --ff-only
 fi
-git clone --depth 1 https://github.com/reubenmiller/go-c8y-cli-addons.git .go-c8y-cli 2>/dev/null
 
 mkdir -p "bash"
 mkdir -p "zsh"
 mkdir -p "fish"
 
+# Use go source code as the binary may not be built yet (i.e. in CI/CD)
 echo "Creating completions: $OUTPUT_DIR"
-c8y completion bash > "bash/c8y"
-c8y completion zsh > "zsh/_c8y"
-c8y completion fish > "fish/c8y.fish"
+go run $ROOT_DIR/cmd/c8y/main.go completion bash > "bash/c8y"
+go run $ROOT_DIR/cmd/c8y/main.go completion zsh > "zsh/_c8y"
+go run $ROOT_DIR/cmd/c8y/main.go completion fish > "fish/c8y.fish"
 
 popd
