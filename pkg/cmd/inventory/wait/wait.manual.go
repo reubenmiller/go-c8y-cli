b35050a749ea87521ddf8f14e9746e3e71401994
reubenmiller <reuben.d.miller@gmail.com>
2021-07-27 20:18:04 +0000

    add c8y inventory wait command

---
+++ pkg/cmd/inventory/wait/wait.manual.go
@@ -0,0 +1,152 @@
+package wait
+
+import (
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/MakeNowJust/heredoc/v2"
+	"github.com/reubenmiller/go-c8y-cli/pkg/c8ywaiter"
+	"github.com/reubenmiller/go-c8y-cli/pkg/cmd/subcommand"
+	"github.com/reubenmiller/go-c8y-cli/pkg/cmderrors"
+	"github.com/reubenmiller/go-c8y-cli/pkg/cmdutil"
+	"github.com/reubenmiller/go-c8y-cli/pkg/completion"
+	"github.com/reubenmiller/go-c8y-cli/pkg/desiredstate"
+	"github.com/reubenmiller/go-c8y-cli/pkg/flags"
+	"github.com/reubenmiller/go-c8y/pkg/c8y"
+	"github.com/spf13/cobra"
+)
+
+type CmdWait struct {
+	*subcommand.SubCommand
+
+	factory *cmdutil.Factory
+
+	ExpectedFragments []string
+}
+
+func NewCmdWait(f *cmdutil.Factory) *CmdWait {
+	ccmd := &CmdWait{
+		factory: f,
+	}
+
+	cmd := &cobra.Command{
+		Use:   "wait",
+		Short: "Wait for managed object",
+		Long:  `Wait for an managed object fragment by polling until a condition is met or a timeout is reached`,
+		Example: heredoc.Doc(`
+			$ c8y inventory wait --id 1234 --fragment "c8y_Mobile.iccd"
+			# Wait for the managed object to have a non-null c8y_Mobile.iccd fragment
+
+			$ c8y inventory wait --id 1234 --fragment '!c8y_Mobile'
+			# Wait for the managed object to not have a c8y_Mobile fragment
+
+			$ c8y inventory wait --id 1234 --fragment 'name=^\d+-\w+$'
+			# Wait for the managed object name fragment to match the regular expression '^\d+-\w+'
+
+			$ c8y inventory wait --id 1234 --fragment 'name=^$' --fragment c8y_IsDevice
+			# Wait for the managed object name fragment to match the regular expression '^\d+-\w+'
+
+			$ c8y inventory wait --id 1234 --duration 1m --interval 10s
+			# Wait for the operation to be set to SUCCESSFUL and give up after 1 minute
+
+			$ c8y inventory list --device 1111 | c8y operations wait --status "FAILED" --status "SUCCESSFUL"
+			# Wait for operation to be set to either FAILED or SUCCESSFUL
+		`),
+		RunE: ccmd.RunE,
+	}
+
+	cmd.SilenceUsage = true
+
+	cmd.Flags().String("id", "", "Inventory id (required) (accepts pipeline)")
+	cmd.Flags().StringSliceVar(&ccmd.ExpectedFragments, "fragments", nil, "Fragments to wait for. If multiple values are given, then it will be applied as an OR operation")
+	cmd.Flags().String("duration", "30s", "Timeout duration. i.e. 30s or 1m (1 minute)")
+	cmd.Flags().String("interval", "5s", "Interval to check on the status, i.e. 10s or 1min")
+	flags.WithOptions(
+		cmd,
+		flags.WithExtendedPipelineSupport("id", "id", true, "deviceId", "source.id", "managedObject.id", "id"),
+	)
+
+	completion.WithOptions(
+		cmd,
+	)
+
+	ccmd.SubCommand = subcommand.NewSubCommand(cmd)
+
+	return ccmd
+}
+
+func (n *CmdWait) RunE(cmd *cobra.Command, args []string) error {
+	cfg, err := n.factory.Config()
+	if err != nil {
+		return err
+	}
+	client, err := n.factory.Client()
+	if err != nil {
+		return err
+	}
+
+	inputIterators, err := cmdutil.NewRequestInputIterators(cmd, cfg)
+	if err != nil {
+		return err
+	}
+
+	duration, err := flags.GetDurationFlag(cmd, "duration", true, time.Second)
+	if err != nil {
+		return err
+	}
+
+	interval, err := flags.GetDurationFlag(cmd, "interval", true, time.Second)
+	if err != nil {
+		return err
+	}
+
+	// path parameters
+	path := flags.NewStringTemplate("{id}")
+	err = flags.WithPathParameters(
+		cmd,
+		path,
+		inputIterators,
+		flags.WithStringValue("id", "id"),
+	)
+	if err != nil {
+		return err
+	}
+
+	state := &c8ywaiter.InventoryState{
+		Client:    client,
+		Fragments: n.ExpectedFragments,
+	}
+
+	totalErrors := 0
+	var lastErr error
+	for {
+		itemID, _, inputErr := path.Execute(false)
+
+		if inputErr == io.EOF {
+			break
+		}
+
+		if totalErrors >= cfg.AbortOnErrorCount() {
+			msg := fmt.Sprintf("Too many errors. total=%d, max=%d. lastError=%s", totalErrors, cfg.AbortOnErrorCount(), lastErr)
+			return cmderrors.NewUserErrorWithExitCode(cmderrors.ExitAbortedWithErrors, msg)
+		}
+
+		state.ID = itemID
+		result, err := desiredstate.WaitFor(interval, duration, state)
+
+		if v, ok := result.(*c8y.ManagedObject); ok {
+			_ = n.factory.WriteJSONToConsole(cfg, cmd, "", []byte(v.Item.Raw))
+		}
+
+		if err != nil {
+			totalErrors++
+			cfg.Logger.Infof("%s", err)
+			lastErr = err
+		}
+	}
+	if totalErrors > 0 {
+		return lastErr
+	}
+	return nil
+}
