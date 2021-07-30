6939bc3cf11b42e1e7171fb9dedce16a51803655
reubenmiller <reuben.d.miller@gmail.com>
2021-07-30 20:06:30 +0000

    refactoring subscription test

--- tests/manual/operations/subscribe.operations.duration.sh
+++ tests/manual/operations/subscribe.operations.duration.sh
@@ -4,13 +4,15 @@ set -eou pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 
-[[ command -v c8y ]] || exit 11
-[[ command -v nohup ]] || exit 12
+debug () { >&2 echo "$@"; }
+command -v c8y >/dev/null || exit 11
+command -v nohup >/dev/null || exit 12
 
 export C8Y_SETTINGS_DEFAULTS_DRY=false
 
 mo_id=$( c8y agents create --template "{name: 'agent_' + _.Char(10)}" --select id --output csv )
-[[ "$mo_id" =~ "^\d$" ]] || exit 10
+debug "mo_id: $mo_id"
+[[ "$mo_id" =~ ^[0-9]+$ ]] || exit 10
 
 TASK_PID=""
 cleanup () {
@@ -22,8 +24,14 @@ trap cleanup EXIT
 
 nohup ./create.operations.sh $mo_id 60 >/dev/null 2>&1 &
 TASK_PID="$!"
+debug "TASK_PID: $TASK_PID"
 
-# starttime=$( date +%s )
 values=$( c8y operations subscribe --device $mo_id --duration 10s || true )
-line_count=$( echo "$values" | grep "^{" | wc -l )
-[[ $line_count -gt 0 ]] || exit 2
+item_count=$( echo "$values" | grep "^{" | wc -l )
+
+# result:
+debug "line_count: $item_count"
+echo "{\"itemCount\":\"$item_count\"}"
+
+# error is 100 + actual line count (to make debugging easier)
+[[ $item_count -gt 0 ]] || exit $(( 100 + $item_count ))
