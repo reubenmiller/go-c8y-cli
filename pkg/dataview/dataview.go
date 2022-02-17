20a87b93ec39821229bab993964891b29ac755fe
reubenmiller <reuben.d.miller@gmail.com>
2022-02-17 12:01:18 +0000

    fix: prevent panics when checking if files or folders exist

--- pkg/dataview/dataview.go
+++ pkg/dataview/dataview.go
@@ -73,8 +73,10 @@ func (v *DataView) LoadDefinitions() error {
 	definitions := make([]Definition, 0)
 	v.Logger.Debugf("Looking for definitions in: %v", v.Paths)
 	for _, path := range v.Paths {
-		if stat, err := os.Stat(path); os.IsNotExist(err) || !stat.IsDir() {
-			v.Logger.Debugf("Skipping view path because it does not exist. path=%s", path)
+		v.Logger.Debugf("Looking for definitions in: %s", path)
+
+		if stat, err := os.Stat(path); err != nil || !(err == nil && stat.IsDir()) {
+			v.Logger.Debugf("Skipping view path because it does not exist. path=%s, error=%s", path, err)
 			continue
 		}
 		err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
