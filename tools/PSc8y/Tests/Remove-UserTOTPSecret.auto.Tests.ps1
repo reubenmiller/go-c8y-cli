9c0d0ae64360301416eabb9b1f76e0a8b3b872ee
Reuben Miller <reuben.d.miller@gmail.com>
2022-03-23 22:42:49 +0100

    feat: c8y users revokeTOTPSecret - revoke a user's TOTP (TFA) secret (#123)

---
+++ tools/PSc8y/Tests/Remove-UserTOTPSecret.auto.Tests.ps1
@@ -0,0 +1,19 @@
+﻿. $PSScriptRoot/imports.ps1
+
+Describe -Name "Remove-UserTOTPSecret" {
+    BeforeEach {
+        $User = PSc8y\New-TestUser
+
+    }
+
+    It -Skip "Revoke a user's TOTP (TFA) secret" {
+        $Response = PSc8y\Remove-UserTFA -Id $User.id
+        $LASTEXITCODE | Should -Be 0
+    }
+
+
+    AfterEach {
+
+    }
+}
+
