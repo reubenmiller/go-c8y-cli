e0d279f199cb2fdc846593df3349453bca8fa510
reubenmiller <reuben.d.miller@gmail.com>
2022-06-23 23:20:13 +0200

    add test for outputFileRaw

---
+++ tests/manual/common/flags/outputFileRaw/tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+export C8Y_SETTINGS_DEFAULTS_DRY=false
+
+createdir () {
+    # Cross-platform compatible
+    local name="${1:-"c8y-temp"}"
+    tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t "$name")
+    echo "$tmpdir"
+}
+
+export TEMP_DIR=$(createdir)
+trap "rm -Rf $TEMP_DIR" EXIT
+
+create_inventory_binary () {
+    local name="$1"
+    local binary_id=
+    echo "Dummy content" > "$TEMP_DIR/binary.txt"
+    binary_id=$(c8y binaries create --file "$TEMP_DIR/binary.txt" --name "$name" --select id -o csv)
+    echo "$binary_id"    
+}
+
+test01 () {
+    binary_id=$(create_inventory_binary "mycustomfilename.py")
+    c8y binaries get --id "$binary_id" --outputFileRaw "$TEMP_DIR/output/prefix-{id}.{filename}" > /dev/null
+    test -f "$TEMP_DIR/output/prefix-$binary_id.mycustomfilename.py"
+}
+
+test01
