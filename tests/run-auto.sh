a93108ad494d7c6c9462a7aa5e948159a8908333
reubenmiller <reuben.d.miller@gmail.com>
2022-08-14 17:58:26 +0200

    add check for commander

--- tests/run-auto.sh
+++ tests/run-auto.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! command -v commander; then
+    echo "commander is not installed!"
+    exit 1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd "$SCRIPT_DIR"
 
