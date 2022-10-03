5c8d8236770f4a336b8392a6f42e42df7dbe179c
reubenmiller <reuben.d.miller@gmail.com>
2022-10-03 19:44:30 +0200

    rename cmdlets using Delete verb to Remove

--- tools/PSc8y/Tests/Remove-Notification2Subscription.auto.Tests.ps1
+++ tools/PSc8y/Tests/Remove-Notification2Subscription.auto.Tests.ps1
@@ -1,12 +1,12 @@
 ﻿. $PSScriptRoot/imports.ps1
 
-Describe -Name "Delete-Notification2Subscription" {
+Describe -Name "Remove-Notification2Subscription" {
     BeforeEach {
 
     }
 
     It -Skip "Delete a subscription" {
-        $Response = PSc8y\Delete-Notification2Subscription -Id 12345
+        $Response = PSc8y\Remove-Notification2Subscription -Id 12345
         $LASTEXITCODE | Should -Be 0
     }
 
