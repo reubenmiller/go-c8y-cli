ec06adfbf4974cd52d7495d1b0c436f4e92faa2a
Reuben Miller <reuben.d.miller@gmail.com>
2024-03-10 10:56:04 +0100

    add auto generated applications versions commands

---
+++ tools/PSc8y/Public/Get-ApplicationVersionCollection.ps1
@@ -0,0 +1,68 @@
+﻿# Code generated from specification version 1.0.0: DO NOT EDIT
+Function Get-ApplicationVersionCollection {
+<#
+.SYNOPSIS
+Get application version collection
+
+.DESCRIPTION
+Get a collection of application versions by a given filter
+
+.LINK
+https://reubenmiller.github.io/go-c8y-cli/docs/cli/c8y/applications_versions_list
+
+.EXAMPLE
+PS> Get-ApplicationVersionCollection -Id 1234
+
+Get application versions
+
+
+#>
+    [cmdletbinding(PositionalBinding=$true,
+                   HelpUri='')]
+    [Alias()]
+    [OutputType([object])]
+    Param(
+        # Application
+        [Parameter(ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [object[]]
+        $Id
+    )
+    DynamicParam {
+        Get-ClientCommonParameters -Type "Get", "Collection"
+    }
+
+    Begin {
+
+        if ($env:C8Y_DISABLE_INHERITANCE -ne $true) {
+            # Inherit preference variables
+            Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        }
+
+        $c8yargs = New-ClientArgument -Parameters $PSBoundParameters -Command "applications versions list"
+        $ClientOptions = Get-ClientOutputOption $PSBoundParameters
+        $TypeOptions = @{
+            Type = "application/vnd.com.nsn.cumulocity.applicationVersionCollection+json"
+            ItemType = "application/vnd.com.nsn.cumulocity.applicationVersion+json"
+            BoundParameters = $PSBoundParameters
+        }
+    }
+
+    Process {
+
+        if ($ClientOptions.ConvertToPS) {
+            $Id `
+            | Group-ClientRequests `
+            | c8y applications versions list $c8yargs `
+            | ConvertFrom-ClientOutput @TypeOptions
+        }
+        else {
+            $Id `
+            | Group-ClientRequests `
+            | c8y applications versions list $c8yargs
+        }
+        
+    }
+
+    End {}
+}
---
+++ tools/PSc8y/Tests/New-ApplicationVersion.auto.Tests.ps1
@@ -0,0 +1,19 @@
+﻿. $PSScriptRoot/imports.ps1
+
+Describe -Name "New-ApplicationVersion" {
+    BeforeEach {
+
+    }
+
+    It -Skip "Create a new application version" {
+        $Response = PSc8y\New-ApplicationVersion -Name $AppName -Key "${AppName}-key" -ContextPath $AppName -Type HOSTED
+        $LASTEXITCODE | Should -Be 0
+        $Response | Should -Not -BeNullOrEmpty
+    }
+
+
+    AfterEach {
+
+    }
+}
+
