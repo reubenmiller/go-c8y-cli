879f8c95b68cf9c5ef796e11c8fdd84d13799e6e
reubenmiller <reuben.d.miller@gmail.com>
2025-01-13 21:56:48 +0100

    feat(remoteaccess): add port-forwarding example

--- pkg/cmd/remoteaccess/connect/run/run.manual.go
+++ pkg/cmd/remoteaccess/connect/run/run.manual.go
@@ -67,6 +67,9 @@ func NewCmdRun(f *cmdutil.Factory) *CmdRun {
 		Example: heredoc.Doc(`
 			$ c8y remoteaccess connect run --device 12345 -- ssh -p %p root@%h
 			Start an interactive SSH session on the device with a given ssh user
+
+			$ c8y remoteaccess connect run --device rpi5-abcdef01 --configuration passthrough -- ssh -p %p -L 1885:127.0.0.1:1883 -o ServerAliveInterval=120 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@127.0.0.1
+			Start an SSH session to setup port-forwarding to map the remote's 127.0.0.1:1883 port to your machine's 1885 port
 		`),
 		RunE: ccmd.RunE,
 	}
