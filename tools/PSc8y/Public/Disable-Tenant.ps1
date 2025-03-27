66e20fbce637dd1df5355f136d906fa092b1b963
reubenmiller <reuben.d.miller@gmail.com>
2025-03-27 23:19:20 +0100

    feat: add tenant enable/disable commands

---
+++ tools/PSc8y/Public/Disable-Tenant.ps1
@@ -0,0 +1,68 @@
+﻿# Code generated from specification version 1.0.0: DO NOT EDIT
+Function Disable-Tenant {
+<#
+.SYNOPSIS
+Disable/Suspend tenant
+
+.DESCRIPTION
+Disable/Suspend an existing tenant
+
+.LINK
+https://reubenmiller.github.io/go-c8y-cli/docs/cli/c8y/tenants_disable
+
+.EXAMPLE
+PS> Disable-Tenant -Id mycompany
+
+Disable a tenant (from the management tenant)
+
+
+#>
+    [cmdletbinding(PositionalBinding=$true,
+                   HelpUri='')]
+    [Alias()]
+    [OutputType([object])]
+    Param(
+        # Tenant id
+        [Parameter(ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [object]
+        $Id
+    )
+    DynamicParam {
+        Get-ClientCommonParameters -Type "Update", "Template"
+    }
+
+    Begin {
+
+        if ($env:C8Y_DISABLE_INHERITANCE -ne $true) {
+            # Inherit preference variables
+            Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        }
+
+        $c8yargs = New-ClientArgument -Parameters $PSBoundParameters -Command "tenants disable"
+        $ClientOptions = Get-ClientOutputOption $PSBoundParameters
+        $TypeOptions = @{
+            Type = "application/vnd.com.nsn.cumulocity.tenant+json"
+            ItemType = ""
+            BoundParameters = $PSBoundParameters
+        }
+    }
+
+    Process {
+
+        if ($ClientOptions.ConvertToPS) {
+            $Id `
+            | Group-ClientRequests `
+            | c8y tenants disable $c8yargs `
+            | ConvertFrom-ClientOutput @TypeOptions
+        }
+        else {
+            $Id `
+            | Group-ClientRequests `
+            | c8y tenants disable $c8yargs
+        }
+        
+    }
+
+    End {}
+}
