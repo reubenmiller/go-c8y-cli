060b5824679d954680bb054bbd5c69798c70bdbc
reubenmiller <reuben.d.miller@gmail.com>
2025-05-31 19:16:42 +0200

    update libraries go-c8y qrterminal and remove go-password

--- pkg/randdata/password.go
+++ pkg/randdata/password.go
@@ -1,47 +1,27 @@
 package randdata
 
-import "github.com/sethvargo/go-password/password"
+import "github.com/reubenmiller/go-c8y/pkg/password"
 
 func Password(total int) string {
-	if total <= 4 {
+	if total <= 8 {
 		total = 32
 	}
-	passwordGen, err := password.NewGenerator(&password.GeneratorInput{
-		// Note: Don't use "#" as it can cause problems if the user stores
-		// the password in a dotenv file
-		// Note: Don't use '"' double quote as some api does not
-		// support parsing it (e.g. bulk device registration api)
-		// Note: Don't use "\" as it just makes escaping more difficult
-		Symbols: "`~!@$%^&*()_|+-=?;:',.<>{}[]/,",
-	})
-
-	if err != nil {
-		return ""
-	}
-
-	if res, err := passwordGen.Generate(total, 2, 2, false, false); err == nil {
-		return res
-	}
-
-	return ""
+	res, _ := password.NewRandomPassword(
+		password.WithLengthConstraints(8, 32),
+		password.WithSymbols(2),
+		password.WithLength(total),
+	)
+	return res
 }
 
 func PasswordURLSafe(total int) string {
-	if total <= 4 {
+	if total <= 8 {
 		total = 32
 	}
-	passwordGen, err := password.NewGenerator(&password.GeneratorInput{
-		// use url query parameter friendly values
-		Symbols: "-._~",
-	})
-
-	if err != nil {
-		return ""
-	}
-
-	if res, err := passwordGen.Generate(total, 2, 2, false, false); err == nil {
-		return res
-	}
-
-	return ""
+	res, _ := password.NewRandomPassword(
+		password.WithLengthConstraints(8, 32),
+		password.WithUrlCompatibleSymbols(2),
+		password.WithLength(total),
+	)
+	return res
 }
