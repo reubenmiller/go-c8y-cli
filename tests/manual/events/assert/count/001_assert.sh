aec98407f444ed029db6185f1b0104165caf4441
reubenmiller <reuben.d.miller@gmail.com>
2026-03-18 15:27:26 +0100

    update system tests

--- tests/manual/events/assert/count/001_assert.sh
+++ tests/manual/events/assert/count/001_assert.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 # set -eou pipefail
 
-mo_id=$( c8y devices create --name "device_with_events" --select id --output csv )
+NAME=$(c8y template execute -n --template "'device_with_events' + _.Char(10)")
+mo_id=$( c8y devices create -f --name "$NAME" --select id --output csv )
 cleanup () {
-    c8y inventory delete --id $mo_id > /dev/null 2>&1 || true
+    c8y inventory delete -n -f --id "$mo_id" > /dev/null 2>&1 || true
 }
 trap cleanup EXIT
 
 # case 1: Strict assertion
-c8y events assert count --device $mo_id --minimum 1 --strict
+c8y events assert count --device "$mo_id" --minimum 1 --strict --attempts 2 --interval 1s
 if [[ $? -ne 112 ]]; then
     exit 1
 fi
 
-c8y events assert count --device $mo_id --minimum 1
+c8y events assert count --device "$mo_id" --minimum 1 --attempts 2 --interval 1s
 if [[ $? -ne 0 ]]; then
     exit 2
 fi
 
-echo "$mo_id" | c8y events create --type c8y_TestEvent --time "-0s" --text "Test alarm"
+echo "$mo_id" | c8y events create -f --type c8y_TestEvent --time "-0s" --text "Test alarm"
 
 # case 2
-echo "$mo_id" | c8y events assert count --minimum 1 | grep "^${mo_id}$"
+echo "$mo_id" | c8y events assert count --minimum 1 --attempts 2 --interval 1s | grep "^${mo_id}$"
 
 # case 3: Filter by type
-echo "$mo_id" | c8y events assert count --type c8y_TestEvent --minimum 1 | grep "^${mo_id}$"
+echo "$mo_id" | c8y events assert count --type c8y_TestEvent --minimum 1 --attempts 2 --interval 1s | grep "^${mo_id}$"
--- tests/manual/measurements/assert/count/001_assert.sh
+++ tests/manual/measurements/assert/count/001_assert.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 # set -eou pipefail
 
-mo_id=$( c8y devices create --name "device_with_measurements" --select id --output csv )
+NAME=$(c8y template execute -n --template "'device_with_measurements' + _.Char(10)")
+mo_id=$( c8y devices create -n -f --name "$NAME" --select id --output csv )
 cleanup () {
-    c8y inventory delete --id $mo_id > /dev/null 2>&1 || true
+    c8y inventory delete -n -f --id "$mo_id" > /dev/null 2>&1 || true
 }
 trap cleanup EXIT
 
 # case 1: Strict assertion
-c8y measurements assert count --device $mo_id --minimum 1 --strict
+c8y measurements assert count --device "$mo_id" --minimum 1 --strict --attempts 2 --interval 1s
 if [[ $? -ne 112 ]]; then
     exit 1
 fi
 
-c8y measurements assert count --device $mo_id --minimum 1
+c8y measurements assert count --device "$mo_id" --minimum 1 --attempts 2 --interval 1s
 if [[ $? -ne 0 ]]; then
     exit 2
 fi
 
-echo "$mo_id" | c8y measurements create --type c8y_TestMeasurement --time "-0s" --template "{c8y_Signal:{sensor01:{value:1.0,unit:'°C'}}}"
+echo "$mo_id" | c8y measurements create -f --type c8y_TestMeasurement --time "-0s" --template "{c8y_Signal:{sensor01:{value:1.0,unit:'°C'}}}"
 
 # case 2
-echo "$mo_id" | c8y measurements assert count --minimum 1 | grep "^${mo_id}$"
+echo "$mo_id" | c8y measurements assert count --minimum 1 --attempts 2 --interval 1s | grep "^${mo_id}$"
 
 # case 3: Filter by type
-echo "$mo_id" | c8y measurements assert count --type c8y_TestMeasurement --minimum 1 | grep "^${mo_id}$"
+echo "$mo_id" | c8y measurements assert count --type c8y_TestMeasurement --minimum 1 --attempts 2 --interval 1s | grep "^${mo_id}$"
